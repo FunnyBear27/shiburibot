730dfb4c10aedeaa78c77645c954bb5b8add83fa
FunnyBear27 <47087854+FunnyBear27@users.noreply.github.com>
2021-05-11 13:13:54 +0300

    А

--- send_notify.py
+++ send_notify.py
@@ -2,7 +2,7 @@ import telebot
 
 
 def send(user_id, note):
-    bot = telebot.TeleBot('1602905794:AAFj2svXMQDKydWN6LETAkTJyAoZLP6XuXw')
+    bot = telebot.TeleBot('')
     bot.send_message(user_id, note)
     bot.send_message(user_id, note)
     bot.send_message(user_id, note)
